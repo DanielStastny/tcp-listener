bc6b90b82fd295e5a0865e9891385002053c21c8
Daniel Stastny <daniel.stastny@gmail.com>
2014-12-16 16:29:31 +0100

    First usable version

---
+++ TCP_Listener/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace TCP_Listener
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonStartListening = new System.Windows.Forms.Button();
+            this.numericUpDownPort = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPort)).BeginInit();
+            this.statusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStartListening
+            // 
+            this.buttonStartListening.Location = new System.Drawing.Point(15, 81);
+            this.buttonStartListening.Name = "buttonStartListening";
+            this.buttonStartListening.Size = new System.Drawing.Size(110, 23);
+            this.buttonStartListening.TabIndex = 0;
+            this.buttonStartListening.Text = "Start listening";
+            this.buttonStartListening.UseVisualStyleBackColor = true;
+            this.buttonStartListening.Click += new System.EventHandler(this.buttonStartListening_Click);
+            // 
+            // numericUpDownPort
+            // 
+            this.numericUpDownPort.Location = new System.Drawing.Point(93, 19);
+            this.numericUpDownPort.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.numericUpDownPort.Name = "numericUpDownPort";
+            this.numericUpDownPort.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDownPort.TabIndex = 1;
+            this.numericUpDownPort.Value = new decimal(new int[] {
+            2112,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Received msg";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 115);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.ManagerRenderMode;
+            this.statusStrip.Size = new System.Drawing.Size(326, 22);
+            this.statusStrip.SizingGrip = false;
+            this.statusStrip.TabIndex = 5;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(59, 17);
+            this.toolStripStatusLabel.Text = "Not ready";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(219, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(202, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(110, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(202, 72);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(110, 32);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 137);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonStartListening);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDownPort);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "TCP Listener";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPort)).EndInit();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartListening;
+        private System.Windows.Forms.NumericUpDown numericUpDownPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ TCP_Listener/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TCP_Listener
+{
+    public partial class Form1 : Form
+    {
+        private const string securityToken = "xuhuixjsoh4s564c456dcsdc48d96c5cs";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private delegate void WriteOut(String str);
+        private delegate void Success();
+        private delegate void SendData(string host, int port, string str);
+
+        private void buttonStartListening_Click(object sender, EventArgs e)
+        {
+            buttonStartListening.Enabled = false; 
+            pictureBox2.Visible = true;
+            numericUpDownPort.Enabled = false;
+            toolStripStatusLabel.Text = "Listens on " + numericUpDownPort.Value.ToString();
+
+            Thread th = new Thread(new ThreadStart(receiveData));
+            th.Start();
+        }
+
+        private void WriteInTxtArea(String str)
+        {
+            textBox1.Text = str;
+        }
+
+        private void receiveData()
+        {
+            TcpListener tcpListener = null;
+
+            string senderHostname = "";
+            string outputText = "";
+
+            int port = Int32.Parse(numericUpDownPort.Value.ToString());
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+
+            try
+            {
+                tcpListener = new TcpListener(ip, port);
+                tcpListener.Start();
+
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                StreamReader sr = new StreamReader(tcpClient.GetStream());
+    
+                while (true)
+                {
+                    string line = sr.ReadLine();
+                    if (line == null || line == "end" || line == "exit")
+                    {
+                        break;
+                    }
+                    else if (line.Contains(securityToken))
+                    {
+                        senderHostname = line.Substring(line.LastIndexOf("#") + 1);
+                    }
+                    else
+                    {
+                        outputText += line;
+                    }
+                }
+
+                sr.Close();
+                tcpClient.Close();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            finally
+            {
+                tcpListener.Stop();
+            }
+
+            Invoke(new WriteOut(WriteInTxtArea), new object[] { outputText });
+            
+            if (senderHostname != "")
+            {
+                Invoke(new SendData(sendData), new object[] { senderHostname, port, outputText }); 
+            }
+
+            Invoke(new Success(signalizeSuccess));
+
+        }
+
+        private void signalizeSuccess()
+        {
+            pictureBox2.Visible = false;
+            pictureBox1.Visible = true;
+            toolStripStatusLabel.Text = "Sucessfull - msg received, answer sent!";
+        }
+
+        private void sendData(string host, int port, string str)
+        {
+            TcpClient tcpCl = null;
+            try
+            {
+                tcpCl = new TcpClient(host, port);
+                NetworkStream ns = tcpCl.GetStream();
+
+                byte[] bytes = Encoding.UTF8.GetBytes(str + Environment.NewLine + securityToken);
+                ns.Write(bytes, 0, bytes.Length);
+
+                ns.Flush();
+                ns.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            finally
+            {
+                tcpCl.Close();
+            }
+        }
+    }
+}
